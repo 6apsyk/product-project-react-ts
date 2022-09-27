195ee0b8faa30e70da7ce6645a2061d9bbe628ae
IliaKuryndin <6apsyk@mail.ru>
2022-09-27 17:32:31 +0300

    настройка абсолютных путей

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { render } from "react-dom";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./app/provider/ThemeProvider";
+import { ThemeProvider } from "app/provider/ThemeProvider";
 
 render(
     <BrowserRouter>
