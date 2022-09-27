36d99e39d8e8f205ef9de6bc8c311e78c2d4b1a0
IliaKuryndin <6apsyk@mail.ru>
2022-09-27 17:47:34 +0300

    настройка fsd архитектуры

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from "react";
 import "./styles/index.scss";
 import { Routes, Route, Link } from "react-router-dom";
-import { AboutPageAsync } from "pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "pages/MainPage/MainPage.async";
-import classNames from "helpers/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import classNames from "shared/lib/classNames/classNames";
 import { useTheme } from "app/provider/ThemeProvider";
 
 const App = () => {
@@ -16,8 +16,8 @@ const App = () => {
             <Link to={"/about"}>О себе</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path="/about" element={<AboutPageAsync />} />
-                    <Route path="/" element={<MainPageAsync />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/" element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from "react-dom";
-import App from "./app/App";
+import App from "app/App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/provider/ThemeProvider";
 
