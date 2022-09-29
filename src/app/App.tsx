2c4240e582e88a81c75b51c4436880d009fee7ba
IliaKuryndin <6apsyk@mail.ru>
2022-09-29 15:34:37 +0300

    добавлен Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import classNames from "shared/lib/classNames/classNames";
 import { useTheme } from "app/provider/ThemeProvider";
 import { AppRouter } from "app/provider/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar/ui";
 
 const App = () => {
     const { theme } = useTheme();
@@ -10,7 +11,10 @@ const App = () => {
     return (
         <div className={classNames("app", {}, [theme])}>
             <Navbar />
-            <AppRouter />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
         </div>
     );
 };
--- src/app/provider/router/ui/AppRouter.tsx
+++ src/app/provider/router/ui/AppRouter.tsx
@@ -7,7 +7,7 @@ function AppRouter() {
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
                 {Object.values(routeConfig).map(({ path, element }) => (
-                    <Route key={path} path={path} element={element} />
+                    <Route key={path} path={path} element={<div className="page-wrapper">{element}</div>} />
                 ))}
             </Routes>
         </Suspense>
