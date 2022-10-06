63ec8dc7263a82f2b6ea3a904fefd2e1edf9ea05
IliaKuryndin <6apsyk@mail.ru>
2022-10-06 19:48:45 +0300

    добавлены переводы

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next'
 import classNames from 'shared/lib/classNames/classNames'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 import cls from './Navbar.module.scss'
@@ -7,14 +8,17 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+
+    const {t} = useTranslation()
+
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.link}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
-                    Главная
+                    {t('Главная')}
                 </AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
-                    О себе
+                    {t('О себе')}
                 </AppLink>
             </div>
         </div>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import classNames from 'shared/lib/classNames/classNames'
 import { Button } from 'shared/ui/Button/Button'
 import { LangSwitcher } from 'widgets/LangSwitcher'
@@ -12,13 +13,15 @@ interface SidebarProps {
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false)
 
+    const {t} = useTranslation()
+
     const   onToggle = () => {
         setCollapsed((prev) => !prev)
     }
 
     return (
         <div data-testid='sidebar' className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <Button data-testid='sidebar-toggle' onClick={onToggle}>toggle</Button>
+            <Button data-testid='sidebar-toggle' onClick={onToggle}>{t('toggle')}</Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
